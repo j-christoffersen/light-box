7fb6e572bacf2e4a15af34a3734d8c8b1302704e
jchristoffersen <jacksontchristo@gmail.com>
2022-11-13 18:10:54 -0800

    wow

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { LedMatrix } from 'rpi-led-matrix';
 import Manager from './Manager';
 
+console.log('what');
 const matrix = new LedMatrix(
   LedMatrix.defaultMatrixOptions(), // TODO
   LedMatrix.defaultRuntimeOptions() // TODO
 );
+console.log('the heck');
 
 const manager = new Manager({ matrix });
 
