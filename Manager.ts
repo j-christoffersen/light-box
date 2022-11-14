64e87736bdf345a4b1b247d297fe72d37a4683c1
jchristoffersen <jacksontchristo@gmail.com>
2022-11-13 18:21:11 -0800

    correct order for sync

--- Manager.ts
+++ Manager.ts
@@ -18,8 +18,6 @@ class Manager {
     nextScene();
     setInterval(nextScene, 20000);
 
-    console.log('< first sync call');
-    matrix.sync();
     matrix.afterSync((matrixx, dt, t) => {
       console.log('> after sync')
       if (scene) {
@@ -28,6 +26,30 @@ class Manager {
       }
       setTimeout(() => matrixx.sync(), 0);
     });
+
+    console.log('< first sync call');
+    
+    matrix
+    .clear() // clear the display
+    .brightness(100) // set the panel brightness to 100%
+    .fgColor(0x0000ff) // set the active color to blue
+    .fill() // color the entire diplay blue
+    .fgColor(0xffff00) // set the active color to yellow
+    // draw a yellow circle around the display
+    .drawCircle(matrix.width() / 2, matrix.height() / 2, matrix.width() / 2 - 1)
+    // draw a yellow rectangle
+    .drawRect(
+      matrix.width() / 4,
+      matrix.height() / 4,
+      matrix.width() / 2,
+      matrix.height() / 2,
+    )
+    // sets the active color to red
+    .fgColor({ r: 255, g: 0, b: 0 })
+    // draw two diagonal red lines connecting the corners
+    .drawLine(0, 0, matrix.width(), matrix.height())
+    .drawLine(matrix.width() - 1, 0, 0, matrix.height() - 1)
+    .sync();
   }
 }
 
